ae9b9bc5c20c24e1505cad370c9ff6e752666813
srravi <ravi@edtek.co>
2019-01-27 17:18:05 +0530

    fg

--- PANDAS/pandas/2_dataframe_basics/pandas_2.py
+++ PANDAS/pandas/2_dataframe_basics/pandas_2.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 
 weather_df = df = pd.read_csv('c://weather_data.csv');
 weather_df['temperature']
+weather_df['day']
 
 
-weather_df['day'] ## weather_df.day
 weather_df[weather_df.temperature >= weather_df["temperature"].mean()]
 weather_df.describe()
 
@@ -27,12 +27,12 @@ weather_df
 
 
 
-
+weather_df.reset_index( inplace=True)
 plt.plot(  weather_df['day'], weather_df['windspeed']  ,label="wind speed" ,alpha=0.2  )
 plt.plot( weather_df['day'], weather_df['temperature'] ,label="temperature" ,alpha=0.8  )
 plt.legend(loc="best")
 
-weather_df['day']
+
 
 plt.xticks([0,1,2,3,4,5] ,["1/1/2017" ,"1/2/2017" ,"1/3/2017" ,"1/4/2017" ,"1/5/2017"])
 plt.bar([0,1,2,3,4,5] ,weather_df['windspeed'] , width =0.5)
