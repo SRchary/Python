2f63c9c01a90c6a8e036ce45bb9ecaeec9c15b51
srravi <ravi@edtek.co>
2019-01-27 16:50:08 +0530

    fg

--- PANDAS/pandas/2_dataframe_basics/pandas_2.py
+++ PANDAS/pandas/2_dataframe_basics/pandas_2.py
@@ -15,3 +15,18 @@ weather_df['day']
 plt.xticks([0,1,2,3,4,5] ,["1/1/2017" ,"1/2/2017" ,"1/3/2017" ,"1/4/2017" ,"1/5/2017"])
 plt.bar([0,1,2,3,4,5] ,weather_df['windspeed'] , width =0.5)
 plt.bar([0,1,2,3,4,5] ,weather_df['temperature'] , width =0.5)
+
+
+
+#####
+weather_df.describe()
+
+plt.xticks([0,1,2,3,4,5] ,["1/1/2017" ,"1/2/2017" ,"1/3/2017" ,"1/4/2017" ,"1/5/2017"])
+plt.bar([0,1,2,3,4,5] ,weather_df['windspeed'] , width =0.5)
+plt.bar([0,1,2,3,4,5] ,weather_df['temperature'] , width =0.5)
+
+plt.plot(  weather_df['day'], weather_df['windspeed']  ,label="wind speed" ,alpha=1 )
+plt.plot( weather_df['day'], weather_df['temperature'] ,label="temperature" ,alpha=0.3  )
+plt.legend(loc="best")
+
+
