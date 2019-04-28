64b472e4dd8a01960d6e1f6fc2a7cfe46e5826ad
python <sr27631@gmail.com>
2019-04-27 18:44:01 -0700

    django project

--- django/ravi/author/models.py
+++ django/ravi/author/models.py
@@ -8,6 +8,10 @@ class AuthorModel(models.Model):
 	author_email =models.CharField(max_length =50)
 	author_address =models.TextField(max_length =250)
 
+	def __unicode__(self):
+		return self.author_name
+
+
 
 
 
