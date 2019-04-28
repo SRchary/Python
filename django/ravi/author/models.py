7341c28c866958ef15ad830009493e6507afe9b8
python <sr27631@gmail.com>
2019-04-27 19:04:27 -0700

    django project

--- django/ravi/author/models.py
+++ django/ravi/author/models.py
@@ -3,12 +3,15 @@ from django.db import models
 # Create your models here.
 
 class AuthorModel(models.Model):
-	author_name =models.CharField(max_length =50)
+	author_name =models.CharField(max_length =50 ,verbose_name ="Author Name" )
 	author_phone =models.CharField(max_length =12)
 	author_email =models.CharField(max_length =50)
 	author_address =models.TextField(max_length =250)
 
 	def __unicode__(self):
+		return self.author_name
+
+	def __str__(self):
 		return self.author_name
 
 
