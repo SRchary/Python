cf40a24f94f27f6d16253bbfd8a9d8d2209e711c
python <sr27631@gmail.com>
2019-04-27 19:37:40 -0700

    django project

--- django/ravi/author/views.py
+++ django/ravi/author/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render ,redirect
 from .models import AuthorModel as author_m
-from . forms import AuthorForm as author_f
+#from . forms import AuthorForm as author_f
 
 
 # Create your views here.
@@ -22,17 +22,10 @@ def add(request):
 def edit(request ,author_id=0):
 	context ={"title":"Authors"}
 	template ="author/edit.html"
+	'''
 	author_details  = author_m.objects.get(id=author_id)
 	form = author_f(request.POST or None, instance=author_details)
 	if(form.is_valid):
-		'''
-		author_details.author_name    = "sf" #request.POST.get('author_name')
-		author_details.author_email   = "sd"#request.POST.get('author_email')
-		author_details.author_phone   = "sdfsd"#request.POST.get('author_phone')
-		author_details.author_address = "dfdfg dfffffff"#request.POST.get('author_address')
-		author_details.save()
-		print(request.POST)
-		'''
 		form.save()
 		redirect("/author")
 	
@@ -40,11 +33,12 @@ def edit(request ,author_id=0):
 		context['author_details'] = author_details
 	else:
 		return redirect("/author/")
-	
+	'''
 	return render(request ,template ,context)
 
 
-def view(request):
+def view(request ,author_id=0):
+	print(author_id)
 	context ={"title":"Authors"}
 	template ="author/edit.html"
 	return render(request ,template ,context)		
