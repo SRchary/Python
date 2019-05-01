9a12614b135192ca103c03659b206f45fee044cd
python <sr27631@gmail.com>
2019-05-01 16:51:48 +0530

    django project

--- django/ravi/author/models.py
+++ django/ravi/author/models.py
@@ -1,12 +1,27 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils import  timezone
+
+
+def Validate_email(email):
+	if "@ravi.edu" not in email:
+		raise ValidationError("This Is not Valid Email Address! This Email is not Contain @ravi.edu ")
+	return email
+
 
 # Create your models here.
 
 class AuthorModel(models.Model):
-	author_name =models.CharField(max_length =50 ,verbose_name ="Author Name" )
-	author_phone =models.CharField(max_length =12)
-	author_email =models.CharField(max_length =50)
-	author_address =models.TextField(max_length =250)
+	author_name     = models.CharField(max_length =50 ,verbose_name ="Author Name" )
+	author_phone    = models.CharField(max_length =12)
+	author_email    = models.EmailField(max_length =50 ,unique=True ,validators=[Validate_email])
+	author_address  = models.TextField(max_length =250)
+	created_on      = models.DateField(auto_now_add =False ,default =timezone.now)
+	updated_on      = models.DateField( auto_now =True ,auto_now_add =False  )
+
+
+
+
 
 	def __unicode__(self):
 		return self.author_name
