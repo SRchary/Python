28040d0d3f60a21ba1590c40a68ad80fa5dc47cd
python <sr27631@gmail.com>
2019-05-01 18:28:40 +0530

    django project

--- django/ravi/author/admin.py
+++ django/ravi/author/admin.py
@@ -2,4 +2,22 @@ from django.contrib import admin
 
 from .models import AuthorModel 
 # Register your models here.
-admin.site.register(AuthorModel)
+
+class AuthorModel_admin(admin.ModelAdmin):
+    fields =[
+        "author_name",
+        "author_phone",
+        "author_email",
+        "author_address",
+        "dob",
+        "created_on",
+        "updated_on",
+
+
+        ]
+    readonly_fields =["created_on","updated_on"]
+
+    class meta:
+        model = AuthorModel
+
+admin.site.register(AuthorModel ,AuthorModel_admin)
