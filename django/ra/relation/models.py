ed7bb1c984e962644ddef72c89a71c3e96fb396f
python <sr27631@gmail.com>
2019-05-13 16:12:05 +0530

    django project

--- django/ra/relation/models.py
+++ django/ra/relation/models.py
@@ -9,11 +9,23 @@ class Company(models.Model):
     def __str__(self):
         return self.name
 
+class Language(models.Model):
+    name =models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.name
+
+
 
 class Employe(models.Model):
     name  = models.CharField(max_length =50)
     employe_id =models.CharField(max_length=10)
     company = models.ForeignKey(Company ,on_delete =models.CASCADE )
+    languages =models.ManyToManyField(Language)
+
+    def __str__(self):
+        return self.name
+
 
 
 
