3f49efbd8a8826c2b05d72b2107a4accd6190668
python <sr27631@gmail.com>
2019-05-13 15:41:07 +0530

    django project

--- django/ra/relation/models.py
+++ django/ra/relation/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Company(models.Model):
+    name    = models.CharField(max_length =20,unique=True)
+    address = models.TextField(max_length = 250)
+
+    def __str__(self):
+        return self.name
+
+
+class Employe(models.Model):
+    name  = models.CharField(max_length =50)
+    employe_id =models.CharField(max_length=10)
+    company = models.ForeignKey(Company ,on_delete =models.CASCADE )
+
+
+
