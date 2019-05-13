e04e3a23140f5215475e9f66f3e3fc0831ba9848
python <sr27631@gmail.com>
2019-05-13 15:41:36 +0530

    django project

--- django/ra/ra/settings.py
+++ django/ra/ra/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "home",
+    "relation"
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'ra.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        'DIRS': ["templates" ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
