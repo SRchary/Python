1e593114f73d0ae3bdf898fa96860d3bebbcdd2e
python <sr27631@gmail.com>
2019-07-20 15:16:59 +0530

    django project

--- PYTHON_BASICS/decoraters.py
+++ PYTHON_BASICS/decoraters.py
@@ -1,17 +1,45 @@
-def decoretor_fun(orginal_fun):
-	
-	def inner_fun():
-		print "IM FROM {}".format(orginal_fun.__name__)
-		return orginal_fun()
-	return inner_fun;
-@decoretor_fun
-def sum():
-	print "TEST"
 
-sum()
+def decoreter_fun(fun):
 
-print "--------------------------------- \n";
-dec = decoretor_fun(sum)
+	def inner():
+		print("X"*20)
+		fun()
+		print("Y"*20)
+
+	return inner
+
+
+def say_hellow():
+
+	print("hellow World")
+
+#Way 1
+# decoretor normal Way
+dec =  decoreter_fun(say_hellow)
+# delete decoreter_fun from memory for closer
+#del decoreter_fun
 dec()
+
+
+#Way 2
+
+@decoreter_fun
+def say_hellow_tow():
+	print("say_hellow_tow ")
+
+say_hellow_tow()
+
+'''
+
+Decoretor is function it takes a function as parameter
+
+Why :  With using decorater we can extend function funcatinality with out dusturbing  a function 
+
+closer also working in decoretor
+
+'''
+
+
+
 
 
