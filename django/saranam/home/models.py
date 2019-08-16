4bd721d1bce701ed35c7ad011b4fdf4cdaa00af8
RAVI <sr27631.com>
2019-08-16 14:58:18 +0530

    kjh

--- django/saranam/home/models.py
+++ django/saranam/home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Languages(models.Model):
+    name= models.CharField(max_length=120, verbose_name="language_name")
+    code = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+    def __unicode__(self):
+        return self.name
+
+class Framework(models.Model):
+    name = models.CharField(max_length=120)
+    feamework_language = models.ForeignKey(Languages,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.feamework_language
+    def __unicode__(self):
+        self.feamework_language
