7f2bef9efbe0f13538a6323b5872aadccb1c6dad
RAVI <sr27631.com>
2019-08-17 22:25:48 +0530

    kjh

--- django/saranam/home/models.py
+++ django/saranam/home/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
+
+class  LanguagesModelManager(models.Manager):
+    def all(self ,*args ,**kwargs):
+        qs = super(LanguagesModelManager, self).all(*args ,**kwargs).filter(active =True)
+        return qs
+
+
 # Create your models here.
 class Languages(models.Model):
     name= models.CharField(max_length=120, verbose_name="language_name")
     code = models.CharField(max_length=120)
+    active = models.BooleanField(default=True)
+   # other = LanguagesModelManager()
 
     def __str__(self):
         return self.name
@@ -12,9 +21,16 @@ class Languages(models.Model):
 
 class Framework(models.Model):
     name = models.CharField(max_length=120)
-    feamework_language = models.ForeignKey(Languages,on_delete=models.CASCADE)
+    framework_language = models.ForeignKey(Languages,on_delete=models.CASCADE)
+    active = models.BooleanField(default=True)
+
 
     def __str__(self):
         return self.feamework_language
     def __unicode__(self):
         self.feamework_language
+
+class  LanguagesModelManager(models.Manager):
+    def all(self ,*args ,**kwargs):
+        qs = super(LanguagesModelManager, self).all(*args ,**kwargs).filter(active =True)
+        return qs
--- django/saranam/home/views.py
+++ django/saranam/home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Languages ,Framework
 def index(request):
-    context_data ={}
+    languages = Languages.objects.all()
+    context_data ={"languages":languages}
     return render(request , "home/index.html" ,context_data )
